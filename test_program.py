0840ae7a3075bd01efaca1c944e8f1db7ebe306d
anofryev <anofryev@gmail.com>
2021-06-02 14:27:54 +0300

    YEC

--- test_program.py
+++ test_program.py
@@ -6,7 +6,7 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 files_list = ['program.py', 'readme.md']
 
 for filename in files_list:
-    assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
+    assert filename in dir_files, f'Файл `{filename}` не найден в корне     sda adasdasdasas sdasdasdasdas репозитория'
 
 try:
     import program
